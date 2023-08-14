3faee473fb3b572a55b3da3c55a72a0e6366faf4
soyeeee2 <soyeeee2@gmail.com>
2023-08-14 18:40:29 +0900

    project setting

---
+++ selenium/crawl.py
@@ -0,0 +1,6 @@
+# # selenium 설치 후 import
+# from selenium import webdriver
+# # Chrome을 조작용 driver로 선언
+# driver = webdriver.Chrome("C:\Users\Soyee\Desktop\chromedriver.exe")
+# # 크롬드라이버로 웹 사이트 접속
+# driver.get('https://www.naver.com/')
